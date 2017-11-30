4f3a6f7bdd8bfc3513e1234185799aa791ab255a
timur.makhmutov <timur.makhmutov@lazada.com>
2017-11-30 16:33:01 +0300

    Added python files

--- python_client.py
+++ python_client.py
@@ -1,1 +1,27 @@
-import customer_pb2, customer_pb2_grpc
+import grpc
+import customer_pb2
+import customer_pb2_grpc
+
+
+channel = grpc.insecure_channel('localhost:50051')
+stub = customer_pb2_grpc.CustomerStub(channel)
+
+request_message1 = customer_pb2.CustomerRequest(id=1,
+                                                name='Vasya')
+response1 = stub.CreateCustomer(request_message1)
+
+print("Customer received: " + str(response1.id))
+print("Customer received: " + str(response1.success))
+
+
+address_message = customer_pb2.CustomerRequest.Address(street='Пресненская набережная',
+                                                       city='Москва',
+                                                       isShippingAddress=True)
+request_message2 = customer_pb2.CustomerRequest(id=2,
+                                                name='Petya',
+                                                addresses=[customer_pb2.CustomerRequest.Address()])
+response2 = stub.CreateCustomer(request_message2)
+
+print("Customer received: " + str(response2.id))
+print("Customer received: " + str(response2.success))
+
